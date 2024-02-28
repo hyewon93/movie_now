3a229dc712abd4e799b39cd605e285a55f341f97
hyewon93 <baehw93@gmail.com>
2024-02-28 13:31:02 -0800

    Update for mobile version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <Stack sx={{ backgroundColor: '#141414', textAlign: 'center', alignItems: 'center', position: 'fixed', bottom: '0px', width: '100%' }}>
-        <Typography mt={15} mb={3} sx={{ color: '#fff', fontSize:'12px' }}>© 2023 HW_DEV. ALL RIGHTS RESERVED.</Typography>
+    <Stack sx={{ backgroundColor: '#141414', textAlign: 'center', alignItems: 'center', bottom: '0px', width: '100%' }}>
+        <Typography mt={3} mb={3} sx={{ color: '#fff', fontSize:'12px' }}>© 2023 HW_DEV. ALL RIGHTS RESERVED.</Typography>
     </Stack>
   )
 }
